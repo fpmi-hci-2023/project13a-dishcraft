18b037aeb87322e2698dd7c1ff237aaa045dd19a
BystrovaV <veronikabystrova94@gmail.com>
2023-09-28 15:04:03 +0300

    Add Hello World

---
+++ DishCraft/src/main/java/com/dishcraft/Controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.dishcraft.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/sayhello")
+    public String sayHelloWorld(){
+        return "Hello World!";
+    }
+    
+}
---
+++ DishCraft/src/test/java/com/dishcraft/DishCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
